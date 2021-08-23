bdb4ac2f33fb0f200a2df16803cbc20b957a9015
faleyy <xmgqgx@gmail.com>
2021-08-23 17:30:57 +0200

    test

--- cardb/urls.py
+++ cardb/urls.py
@@ -1,6 +1,8 @@
+from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auta/', views.auta ,name = "auta")
+    path('auta/', views.auta ,name = "auta"),
+    path('test/', views.test, name= "test")
 ]
--- cardb/views.py
+++ cardb/views.py
@@ -4,3 +4,5 @@ from .models import auto
 def auta(request):
     data = auto.objects.all()
     return render(request, 'auta.html', {"auta":data})
+def test(request):
+    return render(request, 'test.html',{})
