04f88795f60a26a66287f0988a808e1268c56ff0
faleyy <xmgqgx@gmail.com>
2021-08-24 18:33:19 +0200

    css

--- core/settings.py
+++ core/settings.py
@@ -104,18 +104,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if os.getcwd() == '/app':
     import dj_database_url
